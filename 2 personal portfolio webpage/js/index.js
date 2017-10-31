00011a137822d1cfb78293c5592ec91e59d8e469
Abid Hasan <abidhasan@gmail.com>
2017-10-31 00:01:42 -0600

    Portfolio page complete

--- 2 personal portfolio webpage/js/index.js
+++ 2 personal portfolio webpage/js/index.js
@@ -1,23 +1,23 @@
 var active = 0;
 var navs = [];
-
 $(document).ready(function() {
-  $(".nav-link").map(function() { navs.push($(this).attr("data-target"));});
-  
-  $(".nav-link").on("click", function() {
-    showContent($(this).attr("data-target"));
-  });
+  $(".nav-link").map(function() { navs.push($(this).attr("data-target")); });
+
+  $(".nav-link").on("click", function() { showContent($(this).attr("data-target")); });
 });
 
 function showContent(currId) {
-    $(".content-box").addClass("hidden");
-    $("#"+currId).removeClass("hidden");
-    $("#"+currId).addClass("slideInRight");
-    active = navs.indexOf(currId)
+  $(".content-box").addClass("hidden");
+  $("#" + currId).removeClass("hidden");
+  $("#" + currId).addClass("slideInRight");
+  active = navs.indexOf(currId);
+  $(".nav-link").removeClass("active slideInLeft");
+  document.getElementsByClassName("nav-link")[active].className += " slideInLeft active";
 }
 
-$(window).bind('DOMMouseScroll mousewheel', function(e) {
-  active = e.originalEvent.wheelDelta /120 > 0 ? (active - 1) % navs.length : (active + 1) % navs.length;
-  if (active < 0) { active = navs.length -1 ; }
-  showContent(navs[active])
+$(window).bind("DOMMouseScroll mousewheel", function(e) {
+  active =
+    e.originalEvent.wheelDelta / 120 > 0 ? (active - 1) % navs.length : (active + 1) % navs.length;
+  if (active < 0) {active = navs.length - 1; }
+  showContent(navs[active]);
 });
