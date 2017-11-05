3486dc960c81ddcfa9a8d24e469ce5621e72647a
Abid Hasan <abidhasan@gmail.com>
2017-11-05 16:55:05 -0700

    Local Weather project added

---
+++ 4 local weather/js/index.js
@@ -0,0 +1,53 @@
+function formatAMPM(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? 'PM' : 'AM';
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	var strTime = hours + ':' + minutes + ' ' + ampm;
+	return strTime;
+}
+
+function fixWeatherPrefix(weather) {
+	return weather.toLowerCase().replace("clouds", "cloudy");
+}
+
+$(document).ready(function() {
+	window.setTimeout(function() {
+		$(".weather-data div").css("visibility", "visible")
+		//.css({'visibility': 'visible;'});
+	}, 3900);
+	
+	navigator.geolocation.getCurrentPosition(function(pos) {
+		var lat = parseFloat(pos.coords.latitude)
+		var lon = parseFloat(pos.coords.longitude)
+		var url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon;
+		var url2 = 'https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lon + '&date=today&formatted=0';
+
+		$.ajax( {
+			url: url2,
+			success: function(data) {
+				$(".sun-sunrise span").text(formatAMPM(new Date(data.results.sunrise)));
+				$(".sun-sunset span").text(formatAMPM(new Date(data.results.sunset)));
+			},
+			cache: false
+		});
+
+		$.ajax({
+			url: url,
+			success: function(data) {
+				$(".weather-city").text(data.name + ', ' + data.sys.country);
+				$(".weather-temp").text(parseInt(data.main.temp));
+				$(".weather-cond").text(data.weather[0].description);
+				$(".weather-logo .wi").addClass('wi-day-' + fixWeatherPrefix(data.weather[0].main));
+			},
+			cache: false
+		});
+	});
+});
+
+
+$(".weather-temp").on("click", function() {
+	alert(	'hi');
+});
