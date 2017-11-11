8f2978e34ac986ec6cbf20b47c97ccb116e60c5e
Abid Hasan <abidhasan@gmail.com>
2017-11-10 23:53:24 -0700

    Completed Twitch Streamers project

--- 6 twitch streamers/js/index.js
+++ 6 twitch streamers/js/index.js
@@ -1,13 +1,9 @@
-var users = {catstrator: {}, syndicate: {}, riotgames: {}, shroud: {}, nasscomgdc: {}};
+var users = {catstrator: {}, scoga: {}, riotgames: {}, shroud: {}, nasscomgdc: {}, freecodecamp: {}};
 //catstrator: {url: '', bio: '', displayName: '', logo: '', status: '', game: '', text: ''}
 var cat = "all";
 
-$(document).ready(function() {
-  var i = 0;
-    for (user in users) {
-      i++;
-         //get basic user info
-         $.ajax({
+function basicUserData() {
+    return $.ajax({
              url: 'https://wind-bow.glitch.me/twitch-api/users/' + user,
              cache: false
          }).done(function(data) {
@@ -17,25 +13,33 @@ $(document).ready(function() {
              users[data.name].bio = data.bio;
              users[data.name].logo = data.logo == null ? "++++": data.logo;
          });
+}
+
+function advUserData() {
+    $.ajax({
+        url: 'https://wind-bow.glitch.me/twitch-api/streams/' + user,
+        cache: false
+    }).done(function(data) {
+        var currUser = data._links.channel.split("/")[data._links.channel.split("/").length - 1];
+        if (data.stream == null) {
+            //User isnt online
+            users[currUser].game = null;
+            users[currUser].status = 'offline';
+            users[currUser].text = '';
+        } else {
+            users[currUser].status = 'online';
+            users[currUser].game = data.stream.channel.game;  
+            users[currUser].text = data.stream.channel.status;
+        }
+    });
+}
+            
+$(document).ready(function() {
+    for (user in users) {
+        //get basic user info
+        basicUserData();
          //Get current status
-         $.ajax({
-             url: 'https://wind-bow.glitch.me/twitch-api/streams/' + user,
-             cache: false
-         }).done(function(data) {
-             var currUser = data._links.channel.split("/")[data._links.channel.split("/").length - 1];
-             if (data.stream == null) {
-                 //User isnt online
-                 users[currUser].game = null;
-                 users[currUser].status = 'offline';
-                 users[currUser].text = '';
-             } else {
-                 users[currUser].status = 'online';
-                 users[currUser].game = data.stream.channel.game;  
-                 users[currUser].text = data.stream.channel.status;
-             }
-       if (i === 4) {   doFilter() };   
-         });
-      
+        advUserData();  
     }
 });
 
@@ -55,11 +59,13 @@ function doFilter() {
   var written = false;
     $("tbody").html("");
     for (user in users) {
-        //var 
  if ((cat != "all" && users[user].status != cat) || (users[user].displayName.toLowerCase().indexOf($(".search-box").val().toLowerCase()) == -1)) { continue; }
       written = true;
-      console.log()
-      $("tbody").append("<tr><td class=\"table-icon\"><img alt=\"Player Logo\" src=\"" + users[user].logo + "\"></td><td class=\"table-name\">" + users[user].displayName.toLowerCase().replace( $(".search-box").val().toLowerCase(), "<span class='highlight'>" + $(".search-box").val().toLowerCase() + "</span>"  ) + "</td><td class=\"table-status\">" + users[user].text.substr(0, 50) + "</td></tr>");      
+      var app = "<tr class=\"table-" + users[user].status + "\"><td class=\"table-icon\"><img alt=\"Player Logo\" src=\"" + users[user].logo + "\"></td>";
+      app += "<td class=\"table-name\">" + "<a href=\"https://twitch.tv/" + users[user].displayName  + "\">" + users[user].displayName.toLowerCase().replace( $(".search-box").val().toLowerCase(), "<span class='highlight'>" + $(".search-box").val().toLowerCase() + "</span>"  ) + "</a></td>";
+      app += "<td class=\"table-status\">" + (users[user].status == "online" ? "Playing <span class=\"game-title\">" + users[user].text.substr(0, 50) + "</span>" : "Offline") + "</td></tr>";
+      
+      $("tbody").append(app);
     }
     if (written == false) { $("tbody").append("<tr><td class=\"table-no-streamers\">No streamers found</td></tr>"); }
 }
