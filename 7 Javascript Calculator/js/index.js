a2fb073bcf89577e357dca9b4acda539e8f355f1
Abid Hasan <abidhasan@gmail.com>
2017-11-27 20:01:55 -0700

    Completed calculator project

--- 7 Javascript Calculator/js/index.js
+++ 7 Javascript Calculator/js/index.js
@@ -19,7 +19,6 @@ var Calculator = function() {
     ops = ["0"];
     errorState = false;
     lastOp = "";
-    memory = "";
     setDisplay(0);
   }
   
@@ -31,17 +30,35 @@ var Calculator = function() {
       $(".display-memory").addClass("display-inactive");
     }
     
-    if (num < 0) {
+    if (parseFloat(num) < 0) {
      $("#display-minus").removeClass("display-inactive"); 
     } else {
      $("#display-minus").addClass("display-inactive");       
     }
     
-    $(".display-numbers").text(num.toString().replace("-", "").slice(0,10));
+    //if number has "e" in it
+    if (num.toString().indexOf("e") !== -1) {
+      //Is it too small of a number?
+      if (num.toString().split("e")[1].length > 3) {
+        setDisplay("Err");
+        errorState = true;
+        return null;
+      }
+      
+      $("#display-ten").removeClass("display-inactive");
+      $("#display-power").removeClass("display-inactive");
+      $("#display-power").text(num.toString().split("e")[1]);
+    } else {
+      $("#display-ten").addClass("display-inactive");
+      $("#display-power").addClass("display-inactive");
+      $("#display-power").text("00");
+    }
+    
+    $(".display-numbers").text(num.toString().split("e")[0].replace("-", "").slice(0,10));
   }
   
   function doCalc() {
-          //Check for error
+      //Check for error
       var evaluatedExpression = eval(ops.reduce(function(a, v) { return a + v;}));
       console.log(evaluatedExpression)
       if (isNaN(evaluatedExpression) === true) {
@@ -54,15 +71,15 @@ var Calculator = function() {
       //have to repeat prev calc.
       if (ops.length < 3) {
         setDisplay(eval(evaluatedExpression + lastOp));
-        ops = [eval(evaluatedExpression + lastOp)];
-        return null;
+        ops = [eval(evaluatedExpression + lastOp).toString()];
+        return eval(evaluatedExpression + lastOp);
       }
       
       //Do Calc, clear array, set previous calc
       setDisplay(evaluatedExpression);
       lastOp = ops[ops.length-2] + ops[ops.length-1];
       ops = [evaluatedExpression];
-      return evaluatedExpression
+      return evaluatedExpression;
   }
   
   //Initialize calculator
@@ -73,9 +90,8 @@ var Calculator = function() {
     if ((errorState === true || calcOn == false) && (button !== "OFF")) {
       if (button !== "AC") {
         return null;
-      } else {
-        resetCalc();        
       }
+      resetCalc();        
     }
         
     if (/[0-9.]/.test(button)) {
@@ -84,7 +100,10 @@ var Calculator = function() {
       if (functions.indexOf(ops[ops.length-1]) !== -1) {
         ops.push("0");
       }
-      
+      //see if decimal already present
+      if (button === "." && ops[ops.length-1].indexOf(".") !== -1) {
+        return null;
+      }
       this.appendNum(button); 
       
       
@@ -103,7 +122,7 @@ var Calculator = function() {
       //Is it plus minus?
       //Test to see if its numeric, if not then we do nothing
       if (/[0-9]+/.test(ops[ops.length-1])) {
-          ops[ops.length-1] = parseFloat(ops[ops.length-1] * -1);
+          ops[ops.length-1] = parseFloat(ops[ops.length-1] * -1).toString();
           setDisplay(ops[ops.length-1])
       }
       
@@ -111,6 +130,9 @@ var Calculator = function() {
     } else if(button === "OFF") {
       //Calc turned off
       resetCalc();
+      memory = "";
+      $("#display-memory").addClass("display-inactive");
+
       calcOn = false;
       $("#display-minus").addClass("off");
       $("#display-memory").addClass("off");
@@ -148,7 +170,7 @@ var Calculator = function() {
             errorState = true;
             return null;
           }
-          ops[ops.length-1] = Math.sqrt(parseFloat(ops[ops.length-1]));
+          ops[ops.length-1] = Math.sqrt(parseFloat(ops[ops.length-1])).toString();
           setDisplay(ops[ops.length-1]);
           //Because last operation after square root makes no sense
           lastOp = "";
@@ -156,25 +178,52 @@ var Calculator = function() {
       
     
     } else if (button === "=") {
+      //See if they entered an operator with no number:   ==>  '2 +'
+      if (functions.indexOf(ops[ops.length-1]) !== -1) {
+        //remove the useless operator
+        ops.splice(-1, 1);
+        console.log(ops);
+      }
       //Time to compute
       doCalc();
 
     
     } else if (button === "M+") {
+      //If just a number, then lets add it to memory. Otherwise, we need to
+      //Do calculation
+      if (ops.length === 1) {
+        memory = ops[0];
+        $("#display-memory").removeClass("display-inactive");
+        return;
+      }
+      
       //Call equals and add result to memory
       var calculation = doCalc();
+      //null is returned when the calc doesnt work.
       if (calculation !== null) {
         //Store result in memory
         memory = calculation;
+        $("#display-memory").removeClass("display-inactive");
       }
       
       
     } else if (button === "M-") {
-               
-    } else if (button === "MR") {
-      
-    }
+      memory = "";
+      $("#display-memory").addClass("display-inactive");
+
     
+    } else if (button === "MR") {
+      //check if memory
+      if (memory == "") return null;
+      
+      //if final element number, then replace it
+      if (/[0-9]+/.test(ops[ops.length-1])) {
+        ops[ops.length-1] = memory;
+      } else {
+        ops.push(memory);
+      }
+      setDisplay(memory);
+    }
     console.log(ops)
 
     
@@ -188,19 +237,29 @@ var Calculator = function() {
       return null;
     }
     //Othewise, add on the number
-    
-    ops[ops.length - 1] = (ops[ops.length - 1].toString() + num.toString()).replace(/^[0]$/, "");
+    //If only one zero, then replace with number, else add number
+//    ops[ops.length - 1] = ops[ops.length - 1] === "0" ? num.toString() : ops[ops.length - 1].toString() + num.toString();
+    ops[ops.length - 1] = ops[ops.length - 1].toString().replace(/^[0]$/, "") + num.toString();
     setDisplay(ops[ops.length-1]);
   }
-  
-  
 }
 
-var s = new Calculator();
-//console.log(s.buttonPress(2))
-
 $(document).ready(function() {
+  var calc = new Calculator();
+  
+  //key presses
   $("button").click(function() {
-    s.buttonPress($(this).text());
+    calc.buttonPress($(this).text());
+  });
+  //keyboard keys
+  //keydown  27  C
+  $(document).on("keypress", function(e) {
+    var map = {27: "C", 48: "0", 49: "1", 50: "2", 51: "3", 52: "4", 53: "5",
+              54: "6", 55: "7", 56: "8", 57: "9", 46: ".", 45: "-", 43: "+",
+              42: "*", 47: "/", 13: "="}
+    if (map.hasOwnProperty(e.which)) {calc.buttonPress(map[e.which]); }
+  });
+  $(document).keydown(function(e) {
+    if (e.which === 27) calc.buttonPress("C");
   });
 });
